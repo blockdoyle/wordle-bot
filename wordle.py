37ade0004f528330af934fa8250a8bf7c762ffd0
blockdoyle <blockdoyle@gmail.com>
2022-01-22 02:13:53 -0500

    Fixed issue where RegularExpression would only return one word

--- wordle.py
+++ wordle.py
@@ -1,11 +1,44 @@
-import re
+import re, random
 
-file = open("wordlist.txt")
-wordlist = file.read().splitlines()
-file.close()
+def OpenFile(file):
+    f = open(file)
+    return f.read().splitlines()
 
-pattern=input("Enter grep: ")
+def CheckNonRepeating(word):
+    holder = []
+    for char in word:
+        if char in holder:
+            return False,"Word has repeating characters."
+            break
+        else:
+            holder.append(char)
+    return True
 
-for word in wordlist:
-    if re.search(pattern,word):
-        print(word)
+def MakeFiveCharWordlist(words):
+    holder = []
+    for word in words:
+        if len(word) == 5:
+            if CheckNonRepeating(word) == True:
+                holder.append(word)
+            else:
+                pass
+    return holder
+
+def GetRandomNonRepeatingCharWord(words):
+    return random.choice(words)
+
+def RegularExpression(words):
+    pattern = input("Enter RGEX: ")
+    printWords = []
+    for word in words:
+        if re.search(pattern,word):
+            printWords.append(word)
+    return printWords
+
+wordlist = OpenFile('wordlist.txt')
+FiveCharWordlist = MakeFiveCharWordlist(wordlist)
+FirstWord = GetRandomNonRepeatingCharWord(FiveCharWordlist)
+
+print(f"Your first word is: {FirstWord}")
+while True:
+    print(RegularExpression(FiveCharWordlist))
