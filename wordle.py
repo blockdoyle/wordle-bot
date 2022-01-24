bf62ea876ff7587d698bcc258f9077dc06747788
blockdoyle <blockdoyle@gmail.com>
2022-01-24 02:40:37 -0500

    added options to get a new random word and add a new word to dict

--- wordle.py
+++ wordle.py
@@ -4,6 +4,15 @@ def OpenFile(file):
     f = open(file)
     return f.read().splitlines()
 
+def addWord(word,wordlist):
+    f = open('wordlist.txt', 'a')
+    if word not in wordlist:
+        wordlist.append(word)
+        f.write(f'\n{word}')
+        return f"{word} has been added."
+    else:
+        return
+
 def CheckNonRepeating(word):
     holder = []
     for char in word:
@@ -28,11 +37,16 @@ def GetRandomNonRepeatingCharWord(words):
     return random.choice(words)
 
 def RegularExpression(words):
-    pattern = input("\nEnter RGEX: ")
     printWords = []
-    for word in words:
-        if re.search(pattern,word):
-            printWords.append(word)
+    pattern = input("\nEnter RGEX: ")
+    if pattern == "n":
+        printWords.append(GetRandomNonRepeatingCharWord(words))
+    elif pattern == "N":
+        printWords.append(addWord(input("What word to add?: "), wordlist))
+    else:
+        for word in words:
+            if re.search(pattern,word):
+                printWords.append(word)
     return printWords
 
 wordlist = OpenFile('wordlist.txt')
